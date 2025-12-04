b232f167f39135974fc2fe4194695dce1f552502
Lamilaz <lazarejouaniko@gmail.com>
2025-12-04 16:26:48 +0100

    add tokenizer

--- train_tokenizer.py
+++ train_tokenizer.py
@@ -1,23 +1,45 @@
-#IMPORTS
-import tokenizers
-from tokenizers import Tokenizer, normalizers
+from tokenizers import Tokenizer,Regex, normalizers
 from tokenizers.models import BPE
 from tokenizers.trainers import BpeTrainer
 from tokenizers.pre_tokenizers import Whitespace, Sequence, Split, Digits
 from datasets import load_dataset
+import re 
+dataset = load_dataset("rojagtap/bookcorpus", split="train")
 
-# SETUP DATASET
-dataset = load_dataset("mydataset")
+WhiteSpaceWithSplit = Split(
+    pattern=Regex(r"\s+"), # Capture les espaces
+    behavior="merged_with_next"    # Garde l'espace comme un token séparé
+)
 
-# TOKENIZER
-trainer = BpeTrainer(special_tokens=["[UNK]", "[EOS]", "[BOS]", "[PAD]"],max_token_length=10)
-pre_tokenizer = Sequence([Whitespace(), Digits(individual_digits=True) ])
-normalizer = normalizers.Sequence([normalizers.NFD(), normalizers.StripAccents(), normalizers.Lowercase()])
+def get_training_corpus(batch_size=1000):
+    batch = []
+    column_name = "text" 
+    for data in dataset:
+        batch.append(data[column_name])
+        if len(batch) == batch_size:
+            yield batch
+            batch = []
+    if batch:
+        yield batch
+
+trainer = BpeTrainer(
+    special_tokens=["[UNK]", "[EOS]", "[BOS]", "[PAD]"],
+    max_token_length=10,
+    vocab_size=30000 # Pensez à définir une taille de vocabulaire cible
+)
+
+pre_tokenizer = Sequence([WhiteSpaceWithSplit, Digits(individual_digits=True)])
+normalizer = normalizers.Sequence([
+    normalizers.NFC(), 
+    normalizers.StripAccents(), 
+    normalizers.Lowercase()
+])
+
 tokenizer = Tokenizer(BPE(unk_token="[UNK]"))
-
 tokenizer.pre_tokenizer = pre_tokenizer
 tokenizer.normalizer = normalizer
-tokenizer.trainer = trainer
-tokenizer.train_from_iterator(dataset, length=len(dataset)).save("lamilaz_tokenizer.json")
 
+tokenizer.train_from_iterator(get_training_corpus(), trainer=trainer)
 
+tokenizer.save("/home/lamilaz/Documents/Code/POETRY-GPT/lamilaz_tokenizer.json")
+print("Entraînement terminé et tokenizer sauvegardé.")
